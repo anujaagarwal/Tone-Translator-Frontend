59b3babc8588ba56c68a16285538d7321f93008f
Anuja Agarwal <anujaagarwal08@gmail.com>
2024-06-06 18:11:59 +0530

    fix: UI

--- src/components/DisplayResult.jsx
+++ src/components/DisplayResult.jsx
@@ -5,12 +5,11 @@ function DisplayResult({result} ) {
   return (
     <div className="mt-5 p-4 bg-white shadow rounded-lg">
       <h2 className="text-lg font-semibold">Translated Text</h2>
-      {result ? (<div className='block'>
-
-<SmallCard text={`Detected Tone - ${result.tone}` || ''}/>
-<SmallCard text={`Detected Sentiment - ${result.sentiment}`} />
-<SmallCard text={`Translated Text - ${result.translated}`} />
-</div>) : (
+      {result.tone ? (<div className='block'>
+        <SmallCard text={`Detected Tone - ${result.tone}` || ''}/>
+        <SmallCard text={`Detected Sentiment - ${result.sentiment}`} />
+        <SmallCard text={`Translated Text - ${result.translated}`} />
+       </div>) : (
   <p className="mt-2 text-gray-600">No results to display</p>
 
 )}
