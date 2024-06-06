c973d1bea9f5a6361b4696cb956bb72ec7978af2
Anuja Agarwal <anujaagarwal08@gmail.com>
2024-06-06 18:06:25 +0530

    feat: beautified the response

--- src/components/DisplayResult.jsx
+++ src/components/DisplayResult.jsx
@@ -5,11 +5,16 @@ function DisplayResult({result} ) {
   return (
     <div className="mt-5 p-4 bg-white shadow rounded-lg">
       <h2 className="text-lg font-semibold">Translated Text</h2>
-      <div className='block'>
-        <SmallCard text={`Detected Tone - ${result.tone}`}/>
-        <SmallCard text={`Detected Sentiment - ${result.sentiment}`} />
-        <SmallCard text={`Translated Text - ${result.translated}`} />
-      </div>
+      {result ? (<div className='block'>
+
+<SmallCard text={`Detected Tone - ${result.tone}` || ''}/>
+<SmallCard text={`Detected Sentiment - ${result.sentiment}`} />
+<SmallCard text={`Translated Text - ${result.translated}`} />
+</div>) : (
+  <p className="mt-2 text-gray-600">No results to display</p>
+
+)}
+      
       {/* <p className="mt-2 text-gray-600">{result || 'No results to display'}</p> */}
     </div>
   );
